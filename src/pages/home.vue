c4bc7f490630e615ab91462cd527a5835fbc65c1
chiyum <pray870712@gmail.com>
2024-08-10 10:32:51 +0800

    feat: 🎸 data-card元件完成

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,3 +1,30 @@
 <template>
-  <div>home</div>
+  <div>
+    <data-card
+      label="Total Revenue"
+      text="$ 1,000,000"
+      icon="attach_money"
+      btnText="View Details"
+      :callback="
+        () => {
+          console.log('click');
+        }
+      "
+    />
+  </div>
 </template>
+<script setup>
+import dataCard from "@/widgets/ui/data-card.vue";
+import { defineOptions } from "vue";
+
+defineOptions({
+  // layout: "layout-default", // 對應的layout 預設layout-default
+  // isHideSidebar: true, // 是否在slidebar隱藏 預設false
+  sidebarSort: 1, // sidebar排序 預設自動排序 從1開始
+  sidebarIcon: "dashboard", // sidebar icon 若無則也有預設值
+  /** 自定義傳參數選項 */
+  customOptions: {
+    something: "hello world",
+  },
+});
+</script>
---
+++ src/widgets/ui/data-card.vue
@@ -0,0 +1,62 @@
+<template>
+  <q-card class="data-card">
+    <q-card-section class="data-card-top">
+      <q-icon
+        class="data-card-icon"
+        :name="icon || 'attach_money'"
+        color="primary"
+        size="24px"
+      />
+      <div>
+        <h5 class="text-gray-8">{{ label }}</h5>
+        <span>{{ text }}</span>
+      </div>
+    </q-card-section>
+    <q-card-section class="data-card-bottom">
+      <span @click="callback">
+        {{ btnText }}
+      </span>
+    </q-card-section>
+  </q-card>
+</template>
+<script setup>
+defineProps({
+  label: String,
+  text: String,
+  icon: String,
+  btnText: String,
+  callback: Function,
+});
+</script>
+
+<style scoped lang="scss">
+.data-card {
+  &-top {
+    display: flex;
+    align-items: center;
+    padding: 8px;
+    h5 {
+      font-size: 0.83rem;
+      font-weight: 400;
+      letter-spacing: 0.01786em;
+      color: #616161;
+    }
+    span {
+      margin-top: 4px;
+      color: #000000;
+      font-weight: 700;
+      font-size: 1.15rem;
+      letter-spacing: 0.0125em;
+    }
+  }
+  &-icon {
+    padding: 24px;
+  }
+  &-bottom {
+    padding: 16px 26px;
+    color: #1976d2;
+    font-weight: 500;
+    background: #eeeeee;
+  }
+}
+</style>
